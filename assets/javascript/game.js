9496db87827c6ce82816afe7f16dfee7e7766bf3
Joseph Schefer <joseph.schefer@gmail.com>
2017-10-24 11:23:50 -0400

    cleaned up JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
 
 //Declare Variable
-var allAnswers = ["spamalot", "camelot", "holy grail", "king arthur", "knights who say ni", "lancelot", "tim", "holy hand grenade of antioch", "bible", "god", "shrubbery", "coconut", "african swallow", "brave sir robin", "migratory birds", "french", "english", "rabbit", "witch"];
+var allAnswers = ["spamalot", "camelot", "holy grail", "king arthur", "knights who say ni", "lancelot", "tim", "holy hand grenade of antioch", "bible", "god", "shrubbery", "coconut", "african swallow", "brave sir robin", "migratory birds", "french", "english", "witch", "newt", "burn the witch", "watery tart", "sword", "round table", "huge tracts of land", "swamp", "castle", "wooden rabbit", "fart"];
 
 var currentAnswer = [];
 var displayAnswer = [];
@@ -18,12 +18,7 @@ function chooseAnswer() {
   choose = Math.floor(Math.random()*allAnswers.length);
   currentAnswer = allAnswers[choose].split("");
   for (var i = 0; i < allAnswers[choose].length; i++) {
-//    if (allAnswers[choose] === " ") {
-//      displayAnswer.push(" ");
-//    };
-//    else {
     displayAnswer.push("_");
-//    };
   };
 }
 
@@ -37,47 +32,50 @@ function newGuess(guess) {
     wrongAnswers.push(guess);
   };
 }
-
-function removeComma(array) {
-  var str = array.toString();
+// Removes commas from the displayAnswer for cleanliness
+function removeComma(arr) {
+  var str = arr.toString();
   displayStr = str.replace(/,/g, "");
   return displayStr;
 }
-
-function compareArray(array1, array2) {
+// Check to see if the player has won
+function compareArray(arr1, arr2) {
   var test = 0;
-  for (var i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
       test = test + 1;
     };
   };
-  if (test === array1.length) {
+  if (test === arr1.length) {
     winReset();
-    alert("You win!")
   };
 }
 
-function winReset() {
+function reset() {
   currentAnswer.length = 0;
   displayAnswer.length = 0;
   wrongAnswers.length = 0;
-  numWin = numWin + 1;
   chooseAnswer();
+}
+
+function winReset() {
+  alert("You win!")
+  reset();
+  numWin = numWin + 1;
 }
 
 function loseReset() {
   alert("You were turned into a newt! Correct answer was: " + removeComma(currentAnswer));
-  currentAnswer.length = 0;
-  displayAnswer.length = 0;
-  wrongAnswers.length = 0;
+  reset();
   numLoss = numLoss + 1;
-  chooseAnswer();
 }
+
 
 //Functional Actions
 document.onkeypress = function(event) {
   var guess = event.key;
   newGuess(guess);
+  // Displays the current puzzle and wrong answers
   document.getElementById('currentDisplay').innerHTML = removeComma(displayAnswer);
   document.getElementById('wrongAnswers').innerHTML = wrongAnswers.toString();
   //Looks for win condition
@@ -86,6 +84,7 @@ document.onkeypress = function(event) {
   if (wrongAnswers.length > 10) {
     loseReset();
   };
+  // Display wins and losses to the player
   document.getElementById('playerWins').innerHTML = "Wins: " + numWin.toString();
   document.getElementById('playerLoss').innerHTML = "Losses: " + numLoss.toString();
 }
